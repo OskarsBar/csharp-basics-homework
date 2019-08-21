0c6a4d1a741bb427cd68223d460e71cb79efa831
Oskars <oskarsbar@inbox.lv>
2019-08-21 14:01:17 +0300

    initial

---
+++ FlowOfControl/PositiveNegativeNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PositiveNegativeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number.");
+            var input = int.Parse(Console.ReadLine());
+            
+            
+            if (input>0){
+                Console.WriteLine("Number is positive");
+            } else if (input<0) {
+                Console.WriteLine("Number is negative");
+            } else {
+                Console.WriteLine("Number is zero");
+            }
+            
+        }
+    }
+}
