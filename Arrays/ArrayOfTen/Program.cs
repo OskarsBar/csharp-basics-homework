25a81d94bd7ed881ac2a19bfbd169d788a92ebb3
Oskars <oskarsbar@inbox.lv>
2019-08-21 14:08:16 +0300

    initial

---
+++ Arrays/ArrayOfTen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int replaceNum = -7;
+            int[] firstArray=new int[10];
+            int[] secondArray=new int[10];
+
+            initializeArray(firstArray,rand);
+            Console.WriteLine(string.Join(" ", firstArray));
+            copyAndReplace(firstArray, secondArray, replaceNum, secondArray.Length-1);
+            Console.WriteLine(string.Join(" ", secondArray));
+            Console.Read();
+        }
+
+        private static void copyAndReplace(int[] firstArray, int[] secondArray, int replaceNum, int index)
+        {
+            for(int i = 0; i < firstArray.Length; i++)
+            {
+                secondArray[i] = firstArray[i];
+            }
+            secondArray[index] = replaceNum;
+        }
+
+        private static void initializeArray(int[] array,Random rand)
+        {
+            for(int i=0; i<array.Length;i++)
+            {
+                array[i] = rand.Next(1, 100);
+            }
+        }
+    }
+}
---
+++ Arrays/Exercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Exercise3
+{
+    class Program
+    {
+        //TODO: Write a C# program to calculate the average value of array elements.
+        private static void Main(string[] args)
+        {
+            int[] numbers = {20, 30, 25, 35, -16, 60, -100};
+            int sum=0;
+            double avarage = 0;
+            foreach(int i in numbers)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of numbers is {0}", sum);
+            avarage = (double)sum / numbers.Length;
+            Console.WriteLine("avarage is {0}", avarage);
+            /*
+            fixme - calculate sum of all array elements 
+            int sum = 0;
+            for (?){
+                
+            }
+            */
+
+            /*
+            fixme - calculate average value 
+            Console.Write-Line("Average value of the array elements is : " + average);
+            */
+        }
+    }
+}
