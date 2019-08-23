333a4cb95e47d7eaac68388166e5ca8097ee7854
Oskars <oskarsbar@inbox.lv>
2019-08-23 11:35:55 +0300

    initial

---
+++ Arrays/Exercise1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Exercise1
+{
+    class Program
+    {
+        //TODO: Write a C# program to sort a numeric array and a string array.
+        private static void Main(string[] args)
+        {
+            int[] myArray1 = {
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2165, 1457, 2456
+            };
+
+            string[] myArray2 = {
+                "Java",
+                "Python",
+                "PHP",
+                "C#",
+                "C Programming",
+                "C++"
+            };
+            Console.WriteLine("Original number");
+            printArray(myArray1);
+            Array.Sort(myArray1);
+            Console.WriteLine("Sorted  number");
+            printArray(myArray1);
+
+            Console.WriteLine("Original words");
+            printArray(myArray2);
+            Array.Sort(myArray2);
+            Console.WriteLine("Sorted  sorted");
+            printArray(myArray2);
+
+            Console.Read();
+            /*
+            fixme
+            Console.WriteLine("Original numeric array : " + .........);
+            ........... //Sort array
+            Console.WriteLine("Sorted numeric array : " + .........);
+    
+            Console.WriteLine("Original string array : " + .........);
+            ......... //Sort array
+            Console.WriteLine("Sorted string array : " + .........);
+            */
+        }
+
+        private static void printArray(string[] myArray1)
+        {
+            foreach (var year in myArray1)
+            {
+                Console.WriteLine(year);
+            }
+        }
+        private static void printArray(int[] myArray1)
+        {
+            foreach (var year in myArray1)
+            {
+                Console.WriteLine(year);
+            }
+        }
+    }
+}
---
+++ Arrays/Exercise4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        //TODO: Write a C# program to test if an array contains a specific value.
+        private static void Main(string[] args)
+        {
+            bool contains = false;
+            int checkNum = 1245;
+            int[] myArray = {
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2265, 1457, 2456
+            };
+            foreach(int i in myArray)
+            {
+                if (i == checkNum)
+                    contains = true;
+            }
+            if (contains)
+                Console.WriteLine("Contains");
+            else
+                Console.WriteLine("NOT Contains");
+            Console.Read();
+            /*
+            fixme - check if contains number 1245 
+            for (?) {
+                ......
+                Console.WriteLine("Contains!");
+            }
+            */
+        }
+    }
+}
