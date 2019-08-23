8e264fe47f791d0961218b4886f06c61ae86b976
Oskars <oskarsbar@inbox.lv>
2019-08-23 11:30:29 +0300

    initial

---
+++ ArithmeticEx/Uzdevums1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzdevums1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(test(a, b));
+
+            Console.Read();
+        }
+        static bool test(int a, int b)
+        {
+            if (a == 15 || b == 15 || a - b == 15 || a + b == 15)
+                return true;
+            else
+                return false;
+        }
+    }
+}
